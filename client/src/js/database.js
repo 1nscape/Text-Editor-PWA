b13b9780918a0751d20f839aec9f620e906fccfd
Max <max.belenchuk@gmail.com>
2023-04-13 21:27:04 -0300

    Missing code completed and logic added to add content to database and get content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    const JatetxtDB = await openDB('jate', 1);
+    const trans = JatetxtDB.transaction('jate', 'readwrite');
+    const store = trans.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log(result);
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    const JatetxtDB = await openDB('jate', 1);
+    const trans = JatetxtDB.transaction('jate', 'readonly');
+    const store = trans.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+    return result;
+}
 
 initdb();
